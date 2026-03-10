c9b379346e56968b079d90c14e3bde3fb74bde09
Shaik Aftab <shaikaftab1379@gmail.com>
2026-03-10 14:26:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let a = document.getElementById('i1')
+a.addEventListener('click', f1)
+
+let a1 = document.getElementById('i2')
+a1.addEventListener('click', f2)
+
+let a3 = document.getElementById('i3')
+a3.addEventListener('click', f3)
+
+let a4 = document.getElementById('i4')
+a4.addEventListener('click', f4)
+
+let a5 = document.getElementById('i5')
+a5.addEventListener('click', f5)
+
+function f1(){
+    let b = document.getElementById('i1')
+    document.getElementById('cig1').style.background = 'url(Images/checken.jpg)'
+    document.getElementById('cig1').style.backgroundSize = 'cover'
+    document.getElementById('cig1').style.backgroundPosition = 'center'
+
+}
+
+function f2(){
+    let b1 = document.getElementById('i2')
+    document.getElementById('cig1').style.background = 'url(Images/egg.jpg)'
+    document.getElementById('cig1').style.backgroundSize = 'cover'
+    document.getElementById('cig1').style.backgroundPosition = 'center'
+
+}
+
+function f3(){
+    let b3 = document.getElementById('i3')
+    document.getElementById('cig1').style.background = 'url(Images/prawans.jpg)'
+    document.getElementById('cig1').style.backgroundSize = 'cover'
+    document.getElementById('cig1').style.backgroundPosition = 'center'
+
+}
+
+function f4(){
+    let b4 = document.getElementById('i4')
+    document.getElementById('cig1').style.background = 'url(Images/fish.jpg)'
+    document.getElementById('cig1').style.backgroundSize = 'cover'
+    document.getElementById('cig1').style.backgroundPosition = 'center'
+
+}
+
+function f5(){
+    let b5 = document.getElementById('i5')
+    document.getElementById('cig1').style.background = 'url(Images/mutton.jpg)'
+    document.getElementById('cig1').style.backgroundSize = 'cover'
+    document.getElementById('cig1').style.backgroundPosition = 'center'
+
+}
